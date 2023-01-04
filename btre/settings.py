509c85a43526b34a36a8a2dd956816df8e8c1b44
allanstar-byte <allanmutisya263@gmail.com>
2023-01-04 12:03:58 +0300

    Templates/jinja format

--- btre/settings.py
+++ btre/settings.py
@@ -51,11 +51,12 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'btre.urls'
+import os
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1>Hello There</h1>')
+    return render(request, ('pages/index.html'))
+
+def about(request):
+    return render(request, ('pages/about.html'))
 
     

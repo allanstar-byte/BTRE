5aa2758ea0847a3308bae5484d5c56efc1cfde31
allanstar-byte <allanmutisya263@gmail.com>
2023-01-13 15:56:29 +0300

    Listings URLs & Template

--- btre/settings.py
+++ btre/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+        'USER' : 'postgres',
+        'PASSWORD' : 'Allan1,24',
+        'HOST' : 'localhost',
+
     }
 }
 

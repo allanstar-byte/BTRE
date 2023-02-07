fc43207383c82366b2765620baa0b045132e3828
allanstar-byte <allanmutisya263@gmail.com>
2023-02-07 17:21:06 +0300

    Pull Data From Listings Model

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,38 @@
 from django.shortcuts import render
 
-def index(request):
-    return render(request, 'listings/listings.html')
+
+# def index(request):
+#     return render(request, 'listings/listings.html')
 
 def listing(request):
     return render(request, 'listings/listing.html')
 
 def search(request):
     return render(request, 'listings/search.html')
+
+
+#---------------------------------------------------------------
+
+# Reading the data from the database
+
+# import listings model
+
+from .models import Listing
+
+def index(request):
+    listings = Listing.objects.all()
+
+    # incase of an error install (pip install pylint-django)
+
+    # and add this to the settings using ctrl + , ({"python.linting.pylintArgs": [
+    #  "--load-plugins=pylint_django"
+# ],})
+
+# https://stackoverflow.com/questions/45135263/class-has-no-objects-member
+
+    context = {
+        'listings': listings
+    }
+
+    return render(request, 'listings/listings.html', context)
+
